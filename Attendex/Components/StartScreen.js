a45f5aa4a4c2fd57231ac5bea61b952e411605a8
DinosaurPotato534 <shaany0418@gmail.com>
2023-10-05 02:57:07 -0400

    ee
    e

--- Attendex/Components/StartScreen.js
+++ Attendex/Components/StartScreen.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import StartScreen from './StartScreen';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Image, Text, Button } from 'react-native';
+import * as Font from 'expo-font';
 
-export default function App() {
+export default function StartScreen() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    loadCustomFont();
+  }, []);
+
+  async function loadCustomFont() {
+    await Font.loadAsync({
+      'lexend-semi-bold': require('../assets/fonts/Lexend-SemiBold.ttf'),
+    });
+    setFontLoaded(true);
+  }
+
   return (
-    <>
-      <StatusBar style="light" />
-      <StartScreen />
-    </>
+    <View style={styles.container}>
+      {fontLoaded ? (
+        <>
+          <Image style={styles.polygon} source={require('../assets/logo_.png')} />
+          <Text style={styles.welcome}>Welcome!</Text>
+          <Text style={styles.name}>Attendex</Text>
+          <Button style={styles.next}></Button>
+        </>
+      ) : null}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#090909',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  polygon: {
+    width: 365.54,
+    height: 309,
+  },
+
+  welcome: {
+    color: 'white',
+    fontFamily: 'lexend-semi-bold',
+    fontSize: 40,
+    fontWeight: '600',
+    marginTop: 20,
+  },
+
+  name: {
+    color: 'white',
+    fontFamily: 'lexend-semi-bold',
+    fontSize: 16,
+    fontWeight: '300',
+    marginTop: 20,
+  },
+
+  next: {
+    // Add your styles for the button here.
+  }
+});
