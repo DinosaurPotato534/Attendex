d75a18ebaa9756daa9bc3a4ed91ae56afafdd1eb
IntoTheNubiverse <146592434+IntoTheNubiverse@users.noreply.github.com>
2023-10-05 03:30:33 +0000

    Welcome

--- Attendex/App.js
+++ Attendex/App.js
@@ -1,16 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Dimensions,  } from 'react-native';
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lexend&family=Space+Mono&display=swap');
+</style>
 
 export default function App() {
+  console.log("App executed");
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.welcome}>Welcome!</Text>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
+welcome: {
+  color: 'white',
+  width: 194,
+  height: 50,
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: 40,
+  fontFamily: 'Lexend',
+  paddingTop: 0},
+
+
+
+
   container: {
     flex: 1,
     backgroundColor: '#090909',
@@ -18,3 +38,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ 
