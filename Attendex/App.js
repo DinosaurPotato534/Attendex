fdccb66979feb02d24c8a5fb4da929b8075bc60c
DinosaurPotato534 <shaany0418@gmail.com>
2023-10-05 02:28:29 -0400

    e
    ee

--- Attendex/App.js
+++ Attendex/App.js
@@ -17,55 +17,12 @@ export default function App() {
 
 
 const styles = StyleSheet.create({
-  name: {
-    color: 'white',
-    fontFamily: 'lexend',
-    fontWeight: 300,
-    fontSize: 16,
-    height: 26,
-    width: 290,
-    top: 605,
-    left: 50,
-
-  },
-
-
-
-
-  polygon: {
-    width: 365.54,
-    height: 309,
-    alignItems: 'center',
-    justifyContent: 'center',
-    top: 177,
-    left: 17,
-    
-  },
-
-
-
-
-  welcome: {
-    color: 'white',
-    width: 194,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 40,
-    fontFamily: 'lexend',
-    top: 551,
-    left: 98,
-    fontWeight: 600,
-},
-
-
   container: {
     flex: 1,
     backgroundColor: '#090909',
     alignItems: 'center',
     justifyContent: 'center',
   },
-<<<<<<< HEAD
 
   polygon: {
     width: 365.54,
@@ -99,7 +56,4 @@ const styles = StyleSheet.create({
     left: 50,
     marginTop: 20,
   },
-=======
->>>>>>> parent of 4707780 (fixed finns mess)
 });
- 
