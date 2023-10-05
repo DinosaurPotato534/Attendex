e77a7397fe9f501fe57db0b919902fd9dbcd0094
DinosaurPotato534 <shaany0418@gmail.com>
2023-10-05 02:26:01 -0400

    e
    ee

--- Attendex/App.js
+++ Attendex/App.js
@@ -1,41 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
-import * as Font from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Dimensions, Image, Button,  } from 'react-native';
+
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  useEffect(() => {
-    loadCustomFont();
-  }, []);
-
-  async function loadCustomFont() {
-    await Font.loadAsync({
-      'lexend-semi-bold': require('./assets/fonts/Lexend-SemiBold.ttf'),
-    });
-    setFontLoaded(true);
-  }
-
+  console.log("App executed");
+  
   return (
     <View style={styles.container}>
-      {fontLoaded ? (
-        <>
-          <Image style={styles.polygon} source={require('./assets/logo_.png')} />
-          <Text style={styles.welcome}>Welcome!</Text>
-          <Text style={styles.name}>Attendex</Text>
-        </>
-      ) : null}
+      <Text style={styles.welcome}>Welcome!</Text>
+      <Image style={styles.polygon} source={require("./assets/logo_.png")} />
+      <Text style={styles.name}>Attendex</Text>
     </View>
+    
   );
 }
 
+
 const styles = StyleSheet.create({
+  name: {
+    color: 'white',
+    fontFamily: 'lexend',
+    fontWeight: 300,
+    fontSize: 16,
+    height: 26,
+    width: 290,
+    top: 605,
+    left: 50,
+
+  },
+
+
+
+
+  polygon: {
+    width: 365.54,
+    height: 309,
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 177,
+    left: 17,
+    
+  },
+
+
+
+
+  welcome: {
+    color: 'white',
+    width: 194,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 40,
+    fontFamily: 'lexend',
+    top: 551,
+    left: 98,
+    fontWeight: 600,
+},
+
+
   container: {
     flex: 1,
     backgroundColor: '#090909',
     alignItems: 'center',
     justifyContent: 'center',
   },
+<<<<<<< HEAD
 
   polygon: {
     width: 365.54,
@@ -69,4 +99,7 @@ const styles = StyleSheet.create({
     left: 50,
     marginTop: 20,
   },
+=======
+>>>>>>> parent of 4707780 (fixed finns mess)
 });
+ 
